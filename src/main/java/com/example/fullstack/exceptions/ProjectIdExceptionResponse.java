36563b341d6ac277619233bd8e23380f4452c134
Юрій <89320dota@gmail.com>
2018-11-19 07:41:12 +0300

    add my exception

---
+++ src/main/java/com/example/fullstack/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.example.fullstack.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class ProjectIdExceptionResponse {
+    private String projectIdentifier;
+}
--- src/main/java/com/example/fullstack/services/ProjectService.java
+++ src/main/java/com/example/fullstack/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.example.fullstack.services;
 
 import com.example.fullstack.domain.Project;
+import com.example.fullstack.exceptions.ProjectIdException;
 import com.example.fullstack.repositories.ProjectRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
     }
 
 }
