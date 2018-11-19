6d0dcf105bdef5aee13ff1c717290aff89b7eeea
Юрій <89320dota@gmail.com>
2018-11-19 09:02:38 +0300

    add findById

--- src/main/java/com/example/fullstack/services/ProjectService.java
+++ src/main/java/com/example/fullstack/services/ProjectService.java
@@ -23,4 +23,13 @@ public class ProjectService {
 
     }
 
+    public Project findProjectByIdentifer(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null){
+            throw new ProjectIdException("Project ID " + projectId +" not exists");
+        }
+
+        return project;
+    }
 }
